33aa2d62a68bbb075ed52042a123b309078666b1
kyungeun03 <128660836+nuehyk03@users.noreply.github.com>
2023-04-19 19:22:47 +0900

    Update test02.c

--- C_6/test02.c
+++ C_6/test02.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
+    //1부터 n까지의 합 계산
     int i= 1, n, sum=0;
     printf("정수를 입력하시오:");
     scanf("%d", &n);
     
     while(i <= n) {
-        sum+= i; // sum = sim +i;와 같다.
+        sum+= i; // sum = sum +i;와 같다.
         i ++;
     }
     printf("1부터 %d까지의 합은 %d입니다.",n, sum);
