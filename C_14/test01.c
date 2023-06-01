119e724b0d057f99eeb5187baf8f53ba061af884
민경은 <128660836+nuehyk03@users.noreply.github.com>
2023-06-01 12:28:55 +0900

    Add files via upload

---
+++ C_14/test01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+char arr[10001];
+int i,KOI = 0, IOI = 0 ;
+for(i=0; i<strlen(arr)-2; i++){
+  if(arr[i] == 'k' && arr[i+1] == '0' && arr[i+2] == 'I')
+    KOI++;
+  else if(arr[i] == 'I' && arr[i+1] == '0' && arr[i+2] == 'I')
+    IOI++;
+}
+    
+printf("%d\n%d", KOI, IOI);
+return 0;
+
+}
